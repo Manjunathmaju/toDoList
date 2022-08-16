d9210961e119cf7825f98f57274598128fa4c1ec
manjunath_s <manjunaths8301@gmail.com>
2022-08-16 17:44:08 +0530

    chorn: learning to commit

---
+++ scriptToDo.js
@@ -0,0 +1,50 @@
+function getText() {
+    let textValue = document.getElementById('newText').value;
+    console.log(textValue);
+    if (textValue != '') {
+        addTextToList(textValue)
+    }
+}
+
+function addTextToList(text) {
+    let container = document.getElementById('listOfText')
+    let checkBox = document.createElement('input')
+    checkBox.setAttribute('type', 'checkbox')
+    checkBox.setAttribute('id', 'ischeckbox')
+    let removeList = document.createElement('input')
+    removeList.setAttribute('id', 'del')
+    removeList.setAttribute('type', 'button')
+    removeList.setAttribute('value', 'X')
+
+    // funOfAddAttributes('removeList', { 'value': 'x', 'type': 'button' });
+
+    let textList = document.createElement('li')
+    let data = document.createTextNode(text)
+    // unOrderList.setAttribute('id', 'name')
+
+    textList.appendChild(checkBox)
+    textList.appendChild(data);
+    textList.appendChild(removeList)
+
+    // funOfAppendElement('textList', ['removeList', 'data', 'checkbox'])
+
+    container.appendChild(textList)
+}
+
+// function funOfAddAttributes(mainElement, setAtt) {
+//     let key = Object.keys(setAtt)
+//     let value = Object.values(setAtt)
+//     console.log(key.length + ' ' + value.length)
+//     for (let i = key.length; i > 0; i--) {
+//         mainElement = document.setAttribute(`${key[i]}, ${value[i]}`);
+//     }
+//     return;
+// }
+
+
+// function funOfAppendElement(parentNode, appChild) {
+//     for (let i = appChild.length; i > 0; i--) {
+//         parentNode.appendChild(appChild[i])
+//     }
+//     return;
+// }
