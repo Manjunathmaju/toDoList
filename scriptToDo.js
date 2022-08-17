c704a8b6c3ad50bce0d90487ef8b2ac316e9b2b4
manjunath_s <manjunaths8301@gmail.com>
2022-08-17 14:03:32 +0530

    chorn:dotolist face2-classifing the tasks

--- scriptToDo.js
+++ scriptToDo.js
@@ -1,52 +1,58 @@
+let countOfChecked = 0;
+let countOfUnchecked = 0;
+let totatCount = 0;
+
+function counter() {
+    for (let i = totatCount; i >= 0; i--) {
+        if (document.getElementById(toString(i)).checked) {
+            countOfChecked += 1;
+        } else {
+            countOfUnchecked += 1;
+        }
+    }
+}
+
 function getText() {
     let textValue = document.getElementById('newText').value;
-    console.log(textValue);
     if (textValue != '') {
         addTextToList(textValue)
+        counterUpdate()
     }
 }
 
 function addTextToList(text) {
     let container = document.getElementById('listOfText')
     let checkBox = document.createElement('input')
-    checkBox.setAttribute('type', 'checkbox')
-    checkBox.setAttribute('id', 'ischeckbox')
-    let removeList = document.createElement('input')
-    removeList.setAttribute('id', 'del')
-    removeList.setAttribute('type', 'button')
-    removeList.setAttribute('value', 'X')
-
-    // funOfAddAttributes('removeList', { 'value': 'x', 'type': 'button' });
+    funOfAddAttributes(checkBox, { 'value': 'no', 'id': totatCount++, 'type': 'checkbox' });
 
     let textList = document.createElement('li')
     let data = document.createTextNode(text)
-    // unOrderList.setAttribute('id', 'name')
-
-    textList.appendChild(checkBox)
-    textList.appendChild(data);
-    textList.appendChild(removeList)
-
-    // funOfAppendElement('textList', ['removeList', 'data', 'checkbox'])
+    funOfAppendElements(textList, [data, checkBox])
 
     container.appendChild(textList)
 }
 
-// function funOfAddAttributes(mainElement, setAtt) {
-//     let key = Object.keys(setAtt)
-//     let value = Object.values(setAtt)
-//     console.log(key.length + ' ' + value.length)
-//     for (let i = key.length; i > 0; i--) {
-//         mainElement = document.setAttribute(`${key[i]}, ${value[i]}`);
-//     }
-//     return;
-// }
+
+function funOfAddAttributes(mainElement, setAtt) {
+    let key = Object.keys(setAtt)
+    let value = Object.values(setAtt)
+    for (let i = key.length - 1; i >= 0; i--) {
+        mainElement.setAttribute(`${key[i]}`, `${value[i]}`);
+    }
+}
 
 
-// function funOfAppendElement(parentNode, appChild) {
-//     for (let i = appChild.length; i > 0; i--) {
-//         parentNode.appendChild(appChild[i])
-//     }
-//     return;
-// }
+function funOfAppendElements(parentNode, appChild) {
+    for (let i = appChild.length - 1; i >= 0; i--) {
+        parentNode.appendChild(appChild[i])
+    }
+}
 
-//face-2 add the count field, like count the number of tasks completed and uncompleted and total number of tasks in todolist
+function counterUpdate() {
+    // counter();
+    document.getElementById('total').innerText = totatCount;
+    document.getElementById('completed').innerText = countOfChecked;
+    document.getElementById('pending').innerText = countOfUnchecked;
+}
+
+//******face-2************add the count field, like count the number of tasks completed , uncompleted and total number of tasks in todolist
