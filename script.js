6aac707db888ea395b7f05c56a9c71fe7a3d4bbf
Manjunathmanju <8301manju@gmail.com>
2022-08-18 14:39:05 +0530

    function are updated but, bug on appending Child

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ const values = {
     totalTask: 0,
     taskDone: 0,
     taskPending: 0,
-    elem: document.getElementById('masterDiv')
 }
 document.getElementById('addBtn').addEventListener('click', init)
 
@@ -13,33 +12,31 @@ document.getElementById('addBtn').addEventListener('click', init)
 function init() {
     let textValue = document.getElementById('addTask').value;
     if (textValue != '') {
+        values.totalTask += 1;
+        alert(values.totalTask)
         addTextToList(textValue)
-        counterUpdate()
+        // counterUpdate()
     }
 }
 
 function addTextToList(task) {
-    getElement()
-    newElement()
-    addAttributes()
-    appendElements()
+    let newElem = newElement('ul', { 'class': 'allTask' })
+    let newElem2 = newElement('li', { 'class': 'task', 'id': values.totalTask + 1 })
+    let data = document.createTextNode(task);
+    let newElem1 = newElement('input', { 'type': 'checkbox', 'id': values.totalTask })
+    appendElements(newElem2, data, newElem1)
+    let domElem = getElement('class', 'taskList')
+    appendElements(domElem, appendElements(newElem, newElem2))
 }
 
-function setLocalStorage(key, value) {
-    localStorage.setItem(key, value)
+function getElement(prop, value) {
+    let val = (prop === 'id') ? document.getElementById(value) :
+        (prop === 'class') ? document.getElementsByClassName(value) : alert('enter class name or id name!!');
+    return val;
 }
-
-function getLocalScorage() {
-
-}
-
-function getElement(value) {
-    values.elem.
-
-}
-
-function newElement(value) {
-
+function newElement(value, addAtt) {
+    let element = document.createElement(value)
+    return addAttributes(element, addAtt);//here have to check addAtt identifier is object 
 }
 
 function addAttributes(mainElement, setAtt) {
@@ -48,14 +45,31 @@ function addAttributes(mainElement, setAtt) {
     for (let i = key.length - 1; i >= 0; i--) {
         mainElement.setAttribute(`${key[i]}`, `${value[i]}`);
     }
+    return mainElement;
 }
 
 
-function appendElements(parentNode, appChild) {
+function appendElements(parentNode, ...appChild) {
+    console.log('parentnode ' + parentNode)
+    console.log('appChild ' + appChild)
     for (let i = appChild.length - 1; i >= 0; i--) {
-        parentNode.appendChild(appChild[i])
+        if (appChild != undefined) {
+            console.log('inside loop ' + appChild[i])
+            parentNode.appendChild(appChild[i])
+        }
     }
 }
 
+function setLocalStorage(key, value) {
+    localStorage.setItem(key, value)
+}
 
+function getLocalScorage() {
+}
 
+//task-1
+//add tasks to list
+//task-2
+//count the checked tasks
+//task-3
+//push data to local storage
