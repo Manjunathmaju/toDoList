eca7b8fda3a0e1ce6575c9130dd57a005823f061
Manjunathmanju <8301manju@gmail.com>
2022-08-22 18:28:38 +0530

    delete the task and resolve locolstorage proplem

--- script.js
+++ script.js
@@ -18,14 +18,14 @@
             values.totalTask += 1;
             const randomNo = Date.now();
             store[randomNo] = { id: randomNo, task: textValue, status: false };
-            setLocalStorage('localObj', JSON.stringify(store));
             addTextToList(textValue, randomNo);
         }
+        setLocalStorage('localObj', JSON.stringify(store));
     }
 
     function statusUpdate(elem) {
-        const taskId = elem.target.parentNode;
-        console.log(elem)
+        // const taskId = elem.target.parentNode;
+        console.log(elem);
         const currentElementId = elem.target.id
         const localStorageDate = JSON.parse(localStorage.getItem('localObj'));
         console.log(localStorageDate[key])
@@ -41,6 +41,14 @@
             }
         }
     }
+
+    function deleteTask(elem) {
+        let node = elem.target.id
+        node--;
+        console.log(document.querySelector(`#${node}`).remove())
+    }
+
+
     // alert('you clicked the check box');
 
     function addTextToList(task, uniqueNo) {
@@ -49,7 +57,10 @@
         const data = document.createTextNode(task);
         const checkboxElement = createElementFunction('input', { 'type': 'checkbox', 'id': uniqueNo, 'class': 'taskStatus' });
         checkboxElement.addEventListener('click', (e) => { statusUpdate(e) });
+
         const delBtn = createElementFunction('input', { 'class': 'taskDeletBtn', 'type': 'button', 'value': 'delete', 'id': values.totalTask + 1 });
+        delBtn.addEventListener('click', (e) => deleteTask(e))
+
         appendElements(liElement, data, checkboxElement, delBtn);
         const domElement = getElement('class', 'taskList');
         appendElements(ulElement, liElement);
@@ -57,11 +68,13 @@
         let temp = document.querySelectorAll('.taskStatus');
     }
 
+
     function getElement(prop, value) {
         const val = (prop === 'id') ? document.querySelector(`#${value}`) :
             (prop === 'class') ? document.querySelector(`.${value}`) : alert('enter class name or id name!!');
         return val;
     }
+
     function createElementFunction(value, addAtt) {
         const element = document.createElement(value);
         return addAttributes(element, addAtt);//here have to check addAtt identifier is object 
@@ -69,6 +82,7 @@
 
     function addAttributes(mainElement, setAtt) {
         const key = Object.keys(setAtt);
+
         const value = Object.values(setAtt);
         for (let i = key.length - 1; i >= 0; i--) {
             mainElement.setAttribute(`${key[i]}`, `${value[i]}`);
@@ -88,6 +102,7 @@
 
     function getLocalScorage() {
         const localStorageDate = JSON.parse(localStorage.getItem('localObj'));
+
         for (const key in localStorageDate) {
             if (localStorageDate.hasOwnProperty.call(localStorageDate, key)) {
                 const storedText = localStorageDate[key].task;
