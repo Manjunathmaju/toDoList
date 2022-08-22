ae671483d31018a6e99a4deeba1953d702d8ed76
Manjunathmanju <8301manju@gmail.com>
2022-08-22 18:38:46 +0530

    madechanges for review

--- script.js
+++ script.js
@@ -7,106 +7,103 @@
 
     let store = {};
     document.querySelector('.addBtn').addEventListener('click', init);
+
     document.querySelector('.getHistoryTask').addEventListener('click', getLocalScorage);
 
 
     function init() {
-        const textValue = document.querySelector('.addTask').value;
-        if (!textValue) {
+        const textValue = document.querySelector('.addTask').value;//
+        if (!textValue) {//
             alert('enter the task!')
         } else {
-            values.totalTask += 1;
-            const randomNo = Date.now();
-            store[randomNo] = { id: randomNo, task: textValue, status: false };
-            addTextToList(textValue, randomNo);
-        }
-        setLocalStorage('localObj', JSON.stringify(store));
+            values.totalTask += 1;//
+            const randomNo = Date.now();//
+            store[randomNo] = { id: randomNo, task: textValue, status: false };//
+            addTextToList(textValue, randomNo);//
+        }//
+        setLocalStorage('localObj', JSON.stringify(store));//
     }
 
-    function statusUpdate(elem) {
+    function statusUpdate(elem) {//
         // const taskId = elem.target.parentNode;
         console.log(elem);
-        const currentElementId = elem.target.id
-        const localStorageDate = JSON.parse(localStorage.getItem('localObj'));
+        const currentElementId = elem.target.id//
+        const localStorageDate = JSON.parse(localStorage.getItem('localObj'));//
         console.log(localStorageDate[key])
+
         for (const key in localStorageDate) {
-            // console.log(currentElementId)
             if (localStorageDate.hasOwnProperty.call(localStorageDate, key)) {
-                const storedId = localStorageDate[key].id;
-                // console.log(storedId)
+                const storedId = localStorageDate[key].id;//
                 if (storedId === Number(currentElementId)) {
-                    localStorageDate[key].status = true;
+                    localStorageDate[key].status = true;//
                     break;
                 }
             }
         }
     }
 
-    function deleteTask(elem) {
-        let node = elem.target.id
-        node--;
+    function deleteTask(elem) {//
+        let node = elem.target.id//
+        node--;//
         console.log(document.querySelector(`#${node}`).remove())
     }
 
 
     // alert('you clicked the check box');
 
-    function addTextToList(task, uniqueNo) {
-        const ulElement = createElementFunction('ul', { 'class': 'allTask' });
-        const liElement = createElementFunction('li', { 'class': 'task', 'id': values.totalTask });
-        const data = document.createTextNode(task);
-        const checkboxElement = createElementFunction('input', { 'type': 'checkbox', 'id': uniqueNo, 'class': 'taskStatus' });
-        checkboxElement.addEventListener('click', (e) => { statusUpdate(e) });
-
-        const delBtn = createElementFunction('input', { 'class': 'taskDeletBtn', 'type': 'button', 'value': 'delete', 'id': values.totalTask + 1 });
-        delBtn.addEventListener('click', (e) => deleteTask(e))
-
-        appendElements(liElement, data, checkboxElement, delBtn);
-        const domElement = getElement('class', 'taskList');
-        appendElements(ulElement, liElement);
-        appendElements(domElement, ulElement);
-        let temp = document.querySelectorAll('.taskStatus');
+    function addTextToList(task, uniqueNo) {//
+        const ulElement = createElementFunction('ul', { 'class': 'allTask' });//
+        const liElement = createElementFunction('li', { 'class': 'task', 'id': values.totalTask });//
+        const data = document.createTextNode(task);//
+        const checkboxElement = createElementFunction('input', { 'type': 'checkbox', 'id': uniqueNo, 'class': 'taskStatus' });//
+        checkboxElement.addEventListener('click', (e) => { statusUpdate(e) });//
+        const delBtn = createElementFunction('input', { 'class': 'taskDeletBtn', 'type': 'button', 'value': 'delete', 'id': values.totalTask + 1 });//
+        delBtn.addEventListener('click', (e) => deleteTask(e))//
+        appendElements(liElement, data, checkboxElement, delBtn);//
+        const domElement = getElement('class', 'taskList');//
+        appendElements(ulElement, liElement);//
+        appendElements(domElement, ulElement);//
+        let temp = document.querySelectorAll('.taskStatus');//
     }
 
 
-    function getElement(prop, value) {
-        const val = (prop === 'id') ? document.querySelector(`#${value}`) :
-            (prop === 'class') ? document.querySelector(`.${value}`) : alert('enter class name or id name!!');
-        return val;
-    }
+    function getElement(prop, value) {//
+        const val = (prop === 'id') ? document.querySelector(`#${value}`) ://
+            (prop === 'class') ? document.querySelector(`.${value}`) : alert('enter class name or id name!!');//
+        return val;//
+    }//
 
     function createElementFunction(value, addAtt) {
-        const element = document.createElement(value);
+        const element = document.createElement(value);//
         return addAttributes(element, addAtt);//here have to check addAtt identifier is object 
     }
 
     function addAttributes(mainElement, setAtt) {
-        const key = Object.keys(setAtt);
-
-        const value = Object.values(setAtt);
+        const key = Object.keys(setAtt);//
+        const value = Object.values(setAtt);//
         for (let i = key.length - 1; i >= 0; i--) {
             mainElement.setAttribute(`${key[i]}`, `${value[i]}`);
         }
-        return mainElement;
+        return mainElement;//
     }
 
-    function appendElements(parentNode, ...appChild) {
+    function appendElements(parentNode, ...appChild) {//
         for (let i = appChild.length - 1; i >= 0; i--) {
-            parentNode.appendChild(appChild[i]);
+            parentNode.appendChild(appChild[i]);//
         }
     }
 
     function setLocalStorage(key, value) {
-        localStorage.setItem(key, value);
+        localStorage.setItem(key, value);//
     }
 
     function getLocalScorage() {
-        const localStorageDate = JSON.parse(localStorage.getItem('localObj'));
+        const localStorageDate = JSON.parse(localStorage.getItem('localObj'));//
 
         for (const key in localStorageDate) {
             if (localStorageDate.hasOwnProperty.call(localStorageDate, key)) {
-                const storedText = localStorageDate[key].task;
-                addTextToList(storedText);
+                const storedText = localStorageDate[key].task;//
+                addTextToList(storedText);//
             }
         }
     }
