d4a968ed22bf7c98e4a7bcf2969f526714724a9e
Manjunathmanju <8301manju@gmail.com>
2022-08-23 12:21:54 +0530

    checkbos & delete is done & issue in localstore

--- script.js
+++ script.js
@@ -5,73 +5,82 @@
         taskPending: 0
     };
 
-    let store = {};
-    document.querySelector('.addBtn').addEventListener('click', init);
+    const domRender = {
+        addBtn: document.querySelector('.addBtn'),
+        newTask: document.querySelector('.addTask'),
+        getHistoryTasks: document.querySelector('.getHistoryTask'),
+        taskLists: document.querySelector('.taskList')
+    };
 
-    document.querySelector('.getHistoryTask').addEventListener('click', getLocalScorage);
+    const storeOfCurrentTask = {};
+    domRender.addBtn.addEventListener('click', init);
+    domRender.getHistoryTasks.addEventListener('click', getLocalScorage);
 
 
     function init() {
-        const textValue = document.querySelector('.addTask').value;//
-        if (!textValue) {//
+        const textValue = domRender.newTask.value;
+        if (!textValue) {
             alert('enter the task!')
         } else {
-            values.totalTask += 1;//
-            const randomNo = Date.now();//
-            store[randomNo] = { id: randomNo, task: textValue, status: false };//
-            addTextToList(textValue, randomNo);//
-        }//
-        setLocalStorage('localObj', JSON.stringify(store));//
-    }
-
-    function statusUpdate(elem) {//
-        // const taskId = elem.target.parentNode;
-        console.log(elem);
-        const currentElementId = elem.target.id//
-        const localStorageDate = JSON.parse(localStorage.getItem('localObj'));//
-        console.log(localStorageDate[key])
-
-        for (const key in localStorageDate) {
-            if (localStorageDate.hasOwnProperty.call(localStorageDate, key)) {
-                const storedId = localStorageDate[key].id;//
-                if (storedId === Number(currentElementId)) {
-                    localStorageDate[key].status = true;//
-                    break;
-                }
-            }
+            values.totalTask += 1;
+            const randomNo = Date.now();
+            storeOfCurrentTask[randomNo] = { id: randomNo, task: textValue, status: false };
+            addTextToList(textValue, randomNo);
         }
+        localStorage.setItem('localObj', JSON.stringify(storeOfCurrentTask));
     }
 
-    function deleteTask(elem) {//
-        let node = elem.target.id//
-        node--;//
-        console.log(document.querySelector(`#${node}`).remove())
+    function statusUpdate(elem) {
+        const taskId = elem.target.parentNode.id;
+        const localStorageDate = JSON.parse(localStorage.getItem('localObj'));
+        localStorageDate[taskId].status ? localStorageDate[taskId].status = false : localStorageDate[taskId].status = true;
+        localStorage.setItem('localObj', JSON.stringify(localStorageDate));
+        console.log(localStorageDate[taskId].status);
+
+    }
+
+    function deleteTask(elem) {
+        const elementId = elem.target.id
+        const parentElementId = document.getElementById(elementId).parentElement.id
+        const localStorageDate = JSON.parse(localStorage.getItem('localObj'));
+        const wantToDelete = confirm('do you want to delete')
+        if (wantToDelete === true) {
+            delete localStorageDate[parentElementId];
+        } else {
+            console.log('ok no problem');
+        }
+        localStorage.setItem('localObj', JSON.stringify(localStorageDate));
     }
 
 
-    // alert('you clicked the check box');
+    function addTextToList(task, uniqueNo, status) {
+        const ulElement = createElementFunction('ul', { 'class': 'allTask', 'id': uniqueNo });
+        const liElement = createElementFunction('li', { 'class': 'task', 'id': uniqueNo });
+        const data = document.createTextNode(task);
 
-    function addTextToList(task, uniqueNo) {//
-        const ulElement = createElementFunction('ul', { 'class': 'allTask' });//
-        const liElement = createElementFunction('li', { 'class': 'task', 'id': values.totalTask });//
-        const data = document.createTextNode(task);//
-        const checkboxElement = createElementFunction('input', { 'type': 'checkbox', 'id': uniqueNo, 'class': 'taskStatus' });//
-        checkboxElement.addEventListener('click', (e) => { statusUpdate(e) });//
+        let checkboxElement;
+        if (status === undefined) {
+            checkboxElement = createElementFunction('input', { 'type': 'checkbox', 'id': uniqueNo / 1000, 'class': 'taskStatus' });
+        } else if (status) {
+            checkboxElement = createElementFunction('input', { 'type': 'checkbox', 'id': uniqueNo, 'class': 'taskStatus', 'checked': true });//
+        } else {
+            checkboxElement = createElementFunction('input', { 'type': 'checkbox', 'id': uniqueNo, 'class': 'taskStatus' });//
+        }
+
+        checkboxElement.addEventListener('click', (e) => { statusUpdate(e) });
         const delBtn = createElementFunction('input', { 'class': 'taskDeletBtn', 'type': 'button', 'value': 'delete', 'id': values.totalTask + 1 });//
-        delBtn.addEventListener('click', (e) => deleteTask(e))//
-        appendElements(liElement, data, checkboxElement, delBtn);//
-        const domElement = getElement('class', 'taskList');//
-        appendElements(ulElement, liElement);//
-        appendElements(domElement, ulElement);//
-        let temp = document.querySelectorAll('.taskStatus');//
+        delBtn.addEventListener('click', (e) => deleteTask(e))
+        appendElements(liElement, data, checkboxElement, delBtn);
+        appendElements(ulElement, liElement);
+        appendElements(domRender.taskLists, ulElement);
     }
 
 
-    function getElement(prop, value) {//
-        const val = (prop === 'id') ? document.querySelector(`#${value}`) ://
-            (prop === 'class') ? document.querySelector(`.${value}`) : alert('enter class name or id name!!');//
-        return val;//
-    }//
+    function getElement(prop, value) {
+        const updatedProp = (prop === 'id') ? document.querySelector(`#${value}`) :
+            (prop === 'class') ? document.querySelector(`.${value}`) : alert('enter class name or id name!!');
+        return updatedProp;
+    }
 
     function createElementFunction(value, addAtt) {
         const element = document.createElement(value);//
@@ -84,26 +93,25 @@
         for (let i = key.length - 1; i >= 0; i--) {
             mainElement.setAttribute(`${key[i]}`, `${value[i]}`);
         }
-        return mainElement;//
+        return mainElement;
     }
 
-    function appendElements(parentNode, ...appChild) {//
+    function appendElements(parentNode, ...appChild) {
         for (let i = appChild.length - 1; i >= 0; i--) {
-            parentNode.appendChild(appChild[i]);//
+            parentNode.appendChild(appChild[i]);
         }
     }
 
-    function setLocalStorage(key, value) {
-        localStorage.setItem(key, value);//
-    }
-
+    window.onload(getLocalScorage())
     function getLocalScorage() {
-        const localStorageDate = JSON.parse(localStorage.getItem('localObj'));//
+        const localStorageDate = JSON.parse(localStorage.getItem('localObj'));
 
         for (const key in localStorageDate) {
             if (localStorageDate.hasOwnProperty.call(localStorageDate, key)) {
-                const storedText = localStorageDate[key].task;//
-                addTextToList(storedText);//
+                const storedText = localStorageDate[key].task;
+                const storedId = localStorageDate[key].id
+                const storedStatus = localStorageDate[key].status
+                addTextToList(storedText, storedId, storedStatus);
             }
         }
     }
